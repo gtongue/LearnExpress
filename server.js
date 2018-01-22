1f7183df4b836b67806d81773b4129c9d0e47bd5
Garrett Tongue <garrettjtongue@gmail.com>
2018-01-22 15:07:52 -0800

    Easy routes and static pages

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
+//Middleware
+app.use(express.static(__dirname + "/public"));
+
+//Routes
 app.get("/", (req, res) => {
-    res.send('hello express');
+    // res.send('hello express');
+    res.send({
+        name: "Hello",
+        last: "Express"
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.send("This is the about page");
 });
 
 app.listen(3000);
