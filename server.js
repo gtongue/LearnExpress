d93f14231c929884c1b93ef4768b03d3dd03f4d3
Garrett Tongue <garrettjtongue@gmail.com>
2018-01-22 15:17:04 -0800

    Setup Templating Engine (HBS)

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 //Middleware
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + "/public"));
 
 //Routes
 app.get("/", (req, res) => {
     // res.send('hello express');
-    res.send({
-        name: "Hello",
-        last: "Express"
+    // res.send({
+    //     name: "Hello",
+    //     last: "Express"
+    // });
+    res.render("home.hbs", {
+        pageTitle: "Home",
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: "Hello Express!"
     });
 });
 
 app.get("/about", (req, res) => {
-    res.send("This is the about page");
+    // res.send("This is the about page");
+    res.render("about.hbs", {
+        pageTitle: "About Page",
+        currentYear: new Date().getFullYear()
+    });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
