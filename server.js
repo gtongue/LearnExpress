7f53ad6fc7817c2fcbbea62a2781bedabb5096d9
Garrett Tongue <garrettjtongue@gmail.com>
2018-01-22 20:12:07 -0800

    Get ready for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -53,6 +54,7 @@ app.get("/about", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });
